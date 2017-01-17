ec9756efa237bcd6b12295fc8b5b2f1df15cd23d
Ilya Dmitrichenko <errordeveloper@gmail.com>
2017-01-17 11:31:59 +0000

    Refactor pods/pod conversion

--- rubykube/resources_podsClass.go
+++ rubykube/resources_podsClass.go
@@ -15,6 +15,16 @@ func (c *podsClass) getList(ns string, listOptions kapi.ListOptions) (*kapi.PodL
 	return c.rk.clientset.Core().Pods(ns).List(listOptions)
 }
 
+func (c *podsClass) getItem(pods podListTypeAlias, index int) (*podClassInstance, error) {
+	newPodObj, err := c.rk.classes.Pod.New()
+	if err != nil {
+		return nil, err
+	}
+	pod := pods.Items[index]
+	newPodObj.vars.pod = podTypeAlias(pod)
+	return newPodObj, nil
+}
+
 //go:generate gotemplate "./templates/resource/list" "podsListModule(podsClass, \"Pods\", pods, podListTypeAlias)"
 
 func (c *podsClass) defineOwnMethods() {
@@ -22,7 +32,6 @@ func (c *podsClass) defineOwnMethods() {
 	c.rk.appendMethods(c.class, map[string]methodDefintion{
 		"[]": {
 			mruby.ArgsReq(1), func(m *mruby.Mrb, self *mruby.MrbValue) (mruby.Value, mruby.Value) {
-				var pod kapi.Pod
 				vars, err := c.LookupVars(self)
 				if err != nil {
 					return nil, createException(m, err.Error())
@@ -39,25 +48,23 @@ func (c *podsClass) defineOwnMethods() {
 				}
 
 				l := len(vars.pods.Items)
+				i := n.Fixnum()
 
-				if n.Fixnum() >= l {
+				if i >= l {
 					return nil, nil
 				}
 
-				if n.Fixnum() >= 0 {
-					pod = vars.pods.Items[n.Fixnum()]
-				} else if -(l-1) <= n.Fixnum() && n.Fixnum() < 0 {
-					pod = vars.pods.Items[l+n.Fixnum()]
+				if -(l-1) <= i && i < 0 {
+					i = l + i
 				} else {
 					return nil, nil
 				}
 
-				newPodObj, err := c.rk.classes.Pod.New()
+				obj, err := c.getItem(vars.pods, i)
 				if err != nil {
 					return nil, createException(m, err.Error())
 				}
-				newPodObj.vars.pod = podTypeAlias(pod)
-				return newPodObj.self, nil
+				return obj.self, nil
 			},
 			instanceMethod,
 		},
@@ -69,12 +76,11 @@ func (c *podsClass) defineOwnMethods() {
 				}
 
 				if len(vars.pods.Items) > 0 {
-					newPodObj, err := c.rk.classes.Pod.New()
+					obj, err := c.getItem(vars.pods, 0)
 					if err != nil {
 						return nil, createException(m, err.Error())
 					}
-					newPodObj.vars.pod = podTypeAlias(vars.pods.Items[0])
-					return newPodObj.self, nil
+					return obj.self, nil
 				}
 				return nil, nil
 			},
@@ -89,12 +95,11 @@ func (c *podsClass) defineOwnMethods() {
 
 				l := len(vars.pods.Items)
 				if l > 0 {
-					newPodObj, err := c.rk.classes.Pod.New()
+					obj, err := c.getItem(vars.pods, rand.Intn(l))
 					if err != nil {
 						return nil, createException(m, err.Error())
 					}
-					newPodObj.vars.pod = podTypeAlias(vars.pods.Items[rand.Intn(l)])
-					return newPodObj.self, nil
+					return obj.self, nil
 				}
 				return nil, nil
 			},
@@ -110,12 +115,11 @@ func (c *podsClass) defineOwnMethods() {
 				l := len(vars.pods.Items)
 
 				if l > 0 {
-					newPodObj, err := c.rk.classes.Pod.New()
+					obj, err := c.getItem(vars.pods, l-1)
 					if err != nil {
 						return nil, createException(m, err.Error())
 					}
-					newPodObj.vars.pod = podTypeAlias(vars.pods.Items[l-1])
-					return newPodObj.self, nil
+					return obj.self, nil
 				}
 				return nil, nil
 			},
