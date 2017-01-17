8b83db11ef9465e857d5b7451a15365a04e0113d
Ilya Dmitrichenko <errordeveloper@gmail.com>
2017-01-17 14:08:11 +0000

    Generated code

---
+++ rubykube/gotemplate_podSingletonModule.go
@@ -0,0 +1,45 @@
+package rubykube
+
+import (
+	"fmt"
+
+	mruby "github.com/mitchellh/go-mruby"
+)
+
+// template type RubyKubeClass(parentClass, classNameString, instanceVariableName, instanceVariableType)
+
+type podSingletonModule struct{}
+
+func (c *podClass) defineSingletonMethods() {
+	c.rk.appendMethods(c.class, map[string]methodDefintion{
+		"get!": {
+			mruby.ArgsReq(0), func(m *mruby.Mrb, self *mruby.MrbValue) (mruby.Value, mruby.Value) {
+				vars, err := c.LookupVars(self)
+				if err != nil {
+					return nil, createException(m, err.Error())
+				}
+
+				meta := vars.pod.ObjectMeta
+				pod, err := c.getSignleton(meta.Namespace, meta.Name)
+				if err != nil {
+					return nil, createException(m, err.Error())
+				}
+				vars.pod = podTypeAlias(*pod)
+				return self, nil
+			},
+			instanceMethod,
+		},
+		"inspect": {
+			mruby.ArgsReq(0), func(m *mruby.Mrb, self *mruby.MrbValue) (mruby.Value, mruby.Value) {
+				vars, err := c.LookupVars(self)
+				if err != nil {
+					return nil, createException(m, err.Error())
+				}
+
+				fmt.Printf("self: %s/%s\n", vars.pod.ObjectMeta.Namespace, vars.pod.ObjectMeta.Name)
+				return self, nil
+			},
+			instanceMethod,
+		},
+	})
+}
---
+++ rubykube/gotemplate_serviceSignletonModule.go
@@ -0,0 +1,45 @@
+package rubykube
+
+import (
+	"fmt"
+
+	mruby "github.com/mitchellh/go-mruby"
+)
+
+// template type RubyKubeClass(parentClass, classNameString, instanceVariableName, instanceVariableType)
+
+type serviceSignletonModule struct{}
+
+func (c *serviceClass) defineSingletonMethods() {
+	c.rk.appendMethods(c.class, map[string]methodDefintion{
+		"get!": {
+			mruby.ArgsReq(0), func(m *mruby.Mrb, self *mruby.MrbValue) (mruby.Value, mruby.Value) {
+				vars, err := c.LookupVars(self)
+				if err != nil {
+					return nil, createException(m, err.Error())
+				}
+
+				meta := vars.service.ObjectMeta
+				service, err := c.getSignleton(meta.Namespace, meta.Name)
+				if err != nil {
+					return nil, createException(m, err.Error())
+				}
+				vars.service = serviceTypeAlias(*service)
+				return self, nil
+			},
+			instanceMethod,
+		},
+		"inspect": {
+			mruby.ArgsReq(0), func(m *mruby.Mrb, self *mruby.MrbValue) (mruby.Value, mruby.Value) {
+				vars, err := c.LookupVars(self)
+				if err != nil {
+					return nil, createException(m, err.Error())
+				}
+
+				fmt.Printf("self: %s/%s\n", vars.service.ObjectMeta.Namespace, vars.service.ObjectMeta.Name)
+				return self, nil
+			},
+			instanceMethod,
+		},
+	})
+}
