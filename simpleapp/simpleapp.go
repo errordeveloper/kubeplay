08560d12105c8d9428a91dafa805beccaba3dec6
Ilya Dmitrichenko <errordeveloper@gmail.com>
2017-01-24 16:31:55 +0000

    This is a bit more complete now

--- simpleapp/simpleapp.go
+++ simpleapp/simpleapp.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"strings"
 
-	meta "k8s.io/client-go/pkg/api/unversioned" // Should eventually migrate to "k8s.io/apimachinery/pkg/apis/meta/v1"?
+	unversioned "k8s.io/client-go/pkg/api/unversioned" // Should eventually migrate to "k8s.io/apimachinery/pkg/apis/meta/v1"?
 	kapi "k8s.io/client-go/pkg/api/v1"
 	kext "k8s.io/client-go/pkg/apis/extensions/v1beta1"
 )
@@ -19,8 +19,8 @@ type AppComponentOpts struct {
 type AppComponent struct {
 	Image    string
 	Name     string
-	Port     int
-	Replicas int32
+	Port     int32
+	Replicas *int32
 	Opts     AppComponentOpts
 	Env      map[string]string
 	Override interface{}
@@ -42,7 +42,8 @@ type AppComponentResourcePair struct {
 }
 
 const (
-	DEFAULT_REPLICAS = 1
+	DEFAULT_REPLICAS = int32(1)
+	DEFAULT_PORT     = int32(80)
 )
 
 func (i *AppComponent) GetNameAndLabels() (string, map[string]string) {
@@ -72,6 +73,20 @@ func (i *AppComponent) BuildPod(opts AppComponentBuildOpts) *kapi.PodTemplateSpe
 	name, labels := i.GetNameAndLabels()
 	container := kapi.Container{Name: name, Image: i.Image}
 
+	env := []kapi.EnvVar{}
+	for k, v := range i.Env {
+		env = append(env, kapi.EnvVar{Name: k, Value: v})
+	}
+	if len(env) > 0 {
+		container.Env = env
+	}
+
+	port := kapi.ContainerPort{ContainerPort: DEFAULT_PORT}
+	if i.Port != 0 {
+		port.ContainerPort = i.Port
+	}
+	container.Ports = []kapi.ContainerPort{port}
+
 	pod := kapi.PodTemplateSpec{
 		ObjectMeta: kapi.ObjectMeta{
 			Labels: labels,
@@ -89,21 +104,22 @@ func (i *AppComponent) BuildDeployment(opts AppComponentBuildOpts, pod *kapi.Pod
 		return nil
 	}
 
-	replicas := int32(DEFAULT_REPLICAS)
+	meta := i.GetMeta()
+
+	replicas := DEFAULT_REPLICAS
 
 	if i.Replicas != nil {
-		replicas = i.Replicas
+		replicas = *i.Replicas
 	}
 
-	name, labels := i.GetNameAndLabels()
 	deploymentSpec := kext.DeploymentSpec{
 		Replicas: &replicas,
-		Selector: &meta.LabelSelector{MatchLabels: labels},
+		Selector: &unversioned.LabelSelector{MatchLabels: meta.Labels},
 		Template: *pod,
 	}
 
 	deployment := &kext.Deployment{
-		ObjectMeta: i.GetMeta(),
+		ObjectMeta: meta,
 		Spec:       deploymentSpec,
 	}
 
@@ -115,11 +131,22 @@ func (i *AppComponent) BuildDeployment(opts AppComponentBuildOpts, pod *kapi.Pod
 }
 
 func (i *AppComponent) BuildService(opts AppComponentBuildOpts) *kapi.Service {
+	meta := i.GetMeta()
+
+	port := kapi.ServicePort{Port: DEFAULT_PORT}
+	if i.Port != 0 {
+		port.Port = i.Port
+	}
 
 	service := &kapi.Service{
-		ObjectMeta: i.GetMeta(),
+		ObjectMeta: meta,
+		Spec: kapi.ServiceSpec{
+			Ports:    []kapi.ServicePort{port},
+			Selector: meta.Labels,
+		},
 	}
-	return nil
+
+	return service
 }
 
 func (i *AppComponent) Build(opts AppComponentBuildOpts) AppComponentResourcePair {
@@ -135,7 +162,7 @@ func (i *App) Build() []AppComponentResourcePair {
 	opts := AppComponentBuildOpts{Namespace: i.Name}
 	list := []AppComponentResourcePair{}
 
-	for _, service := range i.Components {
+	for _, service := range i.Group {
 		list = append(list, service.Build(opts))
 	}
 
@@ -148,7 +175,7 @@ func main() {
 
 	app := App{
 		Name: "sock-shop",
-		Services: []AppComponent{
+		Group: []AppComponent{
 			{
 				Image: "weaveworksdemos/cart:0.4.0",
 				Opts:  altPromPath,
@@ -164,6 +191,7 @@ func main() {
 			},
 		},
 	}
+
 	for _, resources := range app.Build() {
 		deployment, err := json.MarshalIndent(resources.Deployment, "", "  ")
 		if err != nil {
